02c2471861176afff4b9e112cd932f4c59f0117e
root <marcusbarnesdev@gmail.com>
2020-04-30 15:49:03 -0700

    init test folder

--- server/index.js
+++ server/index.js
@@ -1,36 +1,30 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable eol-last */
 const express = require('express');
+
 const path = require('path');
+
 const app = express();
 const port = 3001;
 const bodyParser = require('body-parser');
 const db = require('./database/config');
-const faker = require('./scripts/dummyData');
 
 
-app.use(express.static(path.join(__dirname,'../public/')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
-
-
-
-app.post('/api/items', (req,res) => {
-  db.createItem(req.body,(err,msg) => {
-    if(err){
+app.use(express.static(path.join(__dirname, '../public/')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/api/items', (req, res) => {
+  db.createItem(req.body, (err) => {
+    if (err) {
       res.status(400);
       res.end();
-    } else{
+    } else {
       res.status(201);
       res.end();
     }
-  })
+  });
 });
-
-
-
-
-
-
-
 app.listen(port, () => {
-  console.log('listening')
+  console.log('listening');
 });
