4396115ee3f454325dc900e32f23a5e0e1ec687b
root <marcusbarnesdev@gmail.com>
2020-04-29 21:10:27 -0700

    created server and database connections

---
+++ server/database/config.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+var uri = 'mongodb://localhost:27017/singleItemPage?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
+mongoose.connect(uri,{useNewUrlParser: true});
+const Schema = mongoose.Schema;
+
+var db = mongoose.connection;
+
+var itemSchema = new Schema({
+  itemId: Number,
+  mainImage: String,
+  altImages: [String]
+});
+
+var Item = mongoose.model('Items', itemSchema);
+
+var createItem = function(obj, cb = () => {}){
+  var item = new Item(obj);
+
+  item.save((err) => {
+    if(err){cb(err,null)}
+    else{cb(null,'congrats')}
+  });
+}
+
+module.exports  = {
+  createItem
+};
