79f68479ae01a928f03fe4a4033a34fabde266b0
root <marcusbarnesdev@gmail.com>
2020-04-29 21:20:05 -0700

    added post api for items

--- server/index.js
+++ server/index.js
@@ -2,13 +2,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3001;
-const mongoose = require('./database/config');
+const bodyParser = require('body-parser');
+const db = require('./database/config');
 
 
 app.use(express.static(path.join(__dirname,'../public/')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 
 
-app.listen(port,() => {
+app.post('/api/items', (req,res) => {
+  db.createItem(req.body,(err,msg) => {
+    if(err){
+      res.status(400);
+      res.end();
+    } else{
+      res.status(201);
+      res.end();
+    }
+  })
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
   console.log('listening')
 });
