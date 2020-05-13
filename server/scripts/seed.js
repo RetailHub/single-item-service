896cd14b2d5368334d924aff9864de11565b3e44
root <marcusbarnesdev@gmail.com>
2020-05-13 12:59:11 -0700

    css and seed changes

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,27 +1,30 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-cond-assign */
 const db = require('../database/config.js');
 const Data = require('./dummyData.js');
 
 
-const clearTables = async function(){
+const clearTables = async function clearTables() {
   await db.deleteAll();
-}
+};
 
 clearTables();
 
-const seedCollection = async function(){
-  var collection = {};
-  var id = 0;
-  while(collection = Data.pop()){
-    try{
-      await db.createItem({itemId:id,altImages:collection.altImages},(err,msg) => {
-        if(err){console.log(err);}
-        else{console.log(msg);}
+const seedCollection = async function seedCollection() {
+  let collection = {};
+  let id = 1;
+  while (collection = Data.pop()) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      await db.createItem({ itemId: id, altImages: collection.altImages }, (err, msg) => {
+        if (err) { console.log(err); } else { console.log(msg); }
       });
       id++;
-    } catch(e){
+    } catch (e) {
       console.log(e);
     }
   }
-}
+};
 
 seedCollection();
