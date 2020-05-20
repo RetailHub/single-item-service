4b8eec85b2a4661c04c1922193bbf5330e02363a
joshskkim <josh.sk.kim@gmail.com>
2020-05-20 15:37:56 -0700

    Created helper functions to generate csvs

--- server/csv/csvhelpers.js
+++ server/csv/csvhelpers.js
@@ -1,9 +1,30 @@
+/* eslint-disable class-methods-use-this */
+
 const dataSources = require('./csvdata.js');
 
-class dataGenerator {
+class DataGenerator {
   constructor() {
     this.altImages = dataSources.altImages;
   }
+
+  getRandomInt(lowerLimit, upperLimit) {
+    return Math.floor(Math.random() * (upperLimit - lowerLimit) + lowerLimit);
+  }
+
+  generateImageArray() {
+    const indexArr = [];
+    const result = [];
+
+    for (let i = 0; i < 5; i += 1) {
+      indexArr.push(this.getRandomInt(i, 100));
+    }
+
+    indexArr.forEach(index => {
+      result.push(this.altImages[index]);
+    });
+
+    return result;
+  }
 }
 
-module.exports = dataGenerator;
+module.exports = DataGenerator;
