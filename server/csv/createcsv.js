2dda4010837c418851bb59ff9d9be48f69bf017a
joshskkim <josh.sk.kim@gmail.com>
2020-05-22 15:44:23 -0700

    Added scripts to seed and create csvs

--- server/csv/createcsv.js
+++ server/csv/createcsv.js
@@ -20,7 +20,7 @@ const writeImages = (writer, encoding, cb) => {
       let itemId = id;
       let altImages = dg.generateImageArray();
 
-      let data = `${itemId}| {${altImages}} \n`;
+      let data = `${itemId}| ${altImages} \n`;
 
       if (i === 0) {
         writer.write(data, encoding, cb);
