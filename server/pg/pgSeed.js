6468439dc00ca0c2e9962b8f51ea4d8945129735
joshskkim <josh.sk.kim@gmail.com>
2020-05-22 15:44:41 -0700

    Refined pg to look at pg specific csv

--- server/pg/pgSeed.js
+++ server/pg/pgSeed.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   port: '5432',
 });
 
-const productImages = path.join(__dirname, '../database/images.csv');
+const productImages = path.join(__dirname, './images.csv');
 
 pool.connect()
   .then(() => console.log('Inserting from CSVs'))
--- server/pg/schema.sql
+++ server/pg/schema.sql
@@ -4,6 +4,8 @@ CREATE DATABASE items;
 
 \c items;
 
+DROP TABLE IF EXISTS items;
+
 CREATE TABLE items (
   itemId SERIAL PRIMARY KEY,
   altImages JSON
