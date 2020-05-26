5370f8d3116d17ca010c9c9c2fb3151e7de637d7
joshskkim <josh.sk.kim@gmail.com>
2020-05-26 13:13:32 -0700

    Client syntax fix

--- client/components/mainImageContainer.jsx
+++ client/components/mainImageContainer.jsx
@@ -9,7 +9,7 @@ const MainImageContainer = (props) => {
     <div className={style.mainImageContainer} onMouseLeave={props.exit}>
       <span>
         <img className={style.mainImage} onMouseMove={props.onHover} onMouseLeave={props.onLeave} src={props.image}/>
-        <div id={style.mags} class={style.none}style={{top:props.y,left:props.x}}>
+        <div id={style.mags} className={style.none}style={{top:props.y,left:props.x}}>
         </div>
       </span>
     </div>
