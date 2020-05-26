03cdc90b076160775dfb4f9555d9754c87d29023
joshskkim <josh.sk.kim@gmail.com>
2020-05-26 14:29:43 -0700

    Reconfigured csv entries

--- server/csv/csvdata.js
+++ server/csv/csvdata.js
@@ -1,7 +1,7 @@
 const arr = [];
 
 for (let i = 0; i < 20; i += 1) {
-  arr.push(`https://sdcimages.s3-us-west-1.amazonaws.com/img${i}.jpg`);
+  arr.push(`'https://sdcimages.s3-us-west-1.amazonaws.com/img${i}.jpg'`);
 }
 
 module.exports = {
