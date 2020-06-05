ce959dbcfb1a357a51ca08a3c5e0b3148c73b6ae
joshskkim <josh.sk.kim@gmail.com>
2020-06-05 16:03:29 -0700

    Added compression

--- server/app.js
+++ server/app.js
@@ -8,10 +8,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const compression = require('compression');
 const bodyParser = require('body-parser');
 // const db = require('./database/config');
 const pgp = require('./pg/pgpIndex.js');
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../public/')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,41 +26,6 @@ app.post('/api/items', (req, res) => {
 // READ ROUTES
 app.get('/api/items/:id', (req, res) => {
   pgp.getImages(req, res);
-  // db.getItem(req.params.id, (err, data) => {
-  //   if (err) {
-  //     res.status(400);
-  //   } else {
-  //     console.log(data);
-  //     res.status(200).send(data);
-  //   }
-  // });
 });
-
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/'));
-// });
-
-// UPDATE ROUTE
-// app.patch('/api/items/:id', (req, res) => {
-//   const { image } = req.body;
-//   db.updateItem(req.params.id, image, (err, data) => {
-//     if (err) {
-//       res.status(404).send('Unable to update');
-//     } else {
-//       res.status(204).send(data);
-//     }
-//   });
-// });
-
-// DELETE ROUTE
-// app.delete('/api/items/:id', (req, res) => {
-//   db.deleteOne(req.params.id, (err, result) => {
-//     if (err) {
-//       res.status(404).send('Unable to delete');
-//     } else {
-//       res.status(200).send(result);
-//     }
-//   });
-// });
 
 module.exports = app;
